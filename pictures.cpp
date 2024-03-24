ccc9efa42f449f190aed1bdd8a09d36f8be6a945
michalinapepel <misia.pepel@onet.pl>
2024-03-24 19:50:23 +0100

    Dodanie pliku pictures.cpp

---
+++ pictures.cpp
@@ -0,0 +1,139 @@
+const char *birdCage = R"(
+                 /\
+             ___/__\___
+            / |  ,_  | \
+           |  | >' ) |  |
+           |  | ( ( \|  |
+           |  | ''|\ |  |
+           |^^^^^^^^^^^^|
+           ``````````````    
+)";
+
+const char *birdFree = R"(
+                 ,_ 
+                >' ) 
+                ( ( \
+                ''|\ 
+)";
+
+const char *six1 = R"(
+                      ooooooooooo
+                    oo          ooo
+                   oo             oo
+                   o               o
+                   o               oo
+                    o             ooo
+                    oo            ooo
+                      oo        oo oo
+                       ooooooooo   oo
+                                  ooo
+                                  o
+                    oo           oo
+                     oo        ooo
+                       ooooooooo
+)";
+const char *six2 = R"(
+                               oooooooo
+                             oo      ooooo
+                           oo            oooo
+                         oo                ooo
+                         o                  ooo
+                         oo                  oo
+                         ooo                oo
+              oo          ooo              oo
+             o             ooo            oo
+            oo               ooooooooooooo
+            oo                       ooo
+             oo                   oooo
+              ooo             ooooo
+                oooo       ooo
+                    ooooooo
+)";
+const char *six3 = R"(
+             oooooo               oooooooooo
+          ooooo              oooooo         oooo
+        ooo                 oo                 oo
+       oo                  oo                   oo
+       oo                 oo                    oo
+        oo                 oo                  oo
+         ooo                oo                oo
+           oooooooo          ooooo        oooo
+                  oooooooooooooooooooooooo     
+)";
+const char *six4 = R"(
+            ooooooo
+          ooooo
+        ooo
+       oo
+       o                  
+       oo                 oooooooooooooo
+       oo              ooo              ooo
+        oo            oo                  oo
+         ooo          o                   oo
+           oooo      oo                    o
+              oooooo  oo                  oo
+                   oooooo                ooo
+                       ooooo            ooo
+                           ooooooooooooo
+)";
+const char *six5 = R"(
+                        ooooooooo
+                      ooo        oo
+                     oo           oo
+                     o
+                    oo
+                    o   ooooooooo
+                   oo oo        oo
+                   ooo            oo
+                   ooo             o
+                   oo               o
+                   oo               o
+                    oo             oo
+                    ooo          oo
+                       ooo    oooo
+                         oooooo
+)";
+const char *six6 = R"(
+                                      ooooooo
+                                   ooo       oooo
+                              ooooo             ooo
+                           oooo                   oo
+                         ooo                       oo
+                       ooooooooooooo               oo
+                     oo            ooo             o
+                    oo              ooo          oo
+                   oo                ooo
+                  oo                  oo
+                  ooo                  o
+                   ooo                oo
+                    oooo            oo
+                       ooooo      oo
+                          oooooooo
+)";
+const char *six7 = R"(
+                 ooooooooooooooooooooooooo
+             oooo        ooooo         ooooooooo
+           oo               oooo               ooo
+          oo                 ooo                 oo
+         oo                    oo                 oo
+         oo                   oo                  oo
+          oo                 oo                 ooo
+           ooooo         ooooo              ooooo
+              ooooooooooo               oooooo
+)";
+const char *six8 = R"(
+                       ooooooooooooo
+                    ooo            ooooo
+                   ooo                oooooo
+                   oo                  oo  oooooo
+                   o                    oo      oooo
+                   oo                   o          ooo
+                   oo                  oo            oo
+                    ooo              ooo              oo
+                       oooooooooooooo                 oo
+                                                       o
+                                                      oo
+                                                    ooo
+                                                ooooo
+                                            ooooooo
+)";
