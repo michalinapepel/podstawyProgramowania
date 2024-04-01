92d6d47cf48dc7b9a39cc9bf1620ede83d991a14
Rafał Krąż <rafalkraz@gmail.com>
2024-04-01 21:48:39 +0200

    Dodanie przykładowego kodu, aktualizacja pliku gitignore

--- terminalGame.cpp
+++ terminalGame.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+#include "funSystem.cpp"
+using namespace std;
+int main()
+{
+  cout << "Tekst1";
+  clearTerminal();
+  cout << "Tekst2";
+  coutNegative("Odwrocony tekst");
+  cout << "Tekst3" << endl;
+  return 0;
+}
