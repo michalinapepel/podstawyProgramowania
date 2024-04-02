67a1bf75ccfe2335f639ec9e2e8f3566890e2eff
michalinapepel <misia.pepel@onet.pl>
2024-04-02 22:05:20 +0200

    Poprawka w pliku funSystem.cpp

---
+++ funSystem.cpp
@@ -0,0 +1,38 @@
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+#include <iostream>
+#include <string>
+using namespace std;
+
+void clearTerminal()
+{
+  cout << "\033[2J\033[1;1H";
+}
+
+void changeColor()
+{
+#if defined(_WIN32)
+  static const HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+  SetConsoleTextAttribute(handle, 240);
+#elif defined(__linux__) || defined(__APPLE__)
+  cout << "\033[30;107m";
+#endif
+}
+
+void colorReset()
+{
+#if defined(_WIN32)
+  static const HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+  SetConsoleTextAttribute(handle, 7);
+#elif defined(__linux__) || defined(__APPLE__)
+  cout << "\033[0m";
+#endif
+}
+
+void coutNegative(const string &s)
+{
+  changeColor();
+  cout << s;
+  colorReset();
+}
