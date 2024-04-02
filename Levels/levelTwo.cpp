7843fa07c22ad4fa9e2985a4f8122405b10d956b
michalinapepel <misia.pepel@onet.pl>
2024-04-02 22:48:34 +0200

    Dodanie części kodu

---
+++ Levels/levelTwo.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "../pictures.cpp"
+#include "../funSystem.cpp"
+
+using namespace std;
+int levelTwo(){
+    int score = 0;
+    string answer = "";
+    int count = 0;
+    do {
+        clearTerminal();
+        cout << "POZIOM 2\n\n";
+        cout << birdCage;
+        if(count == 0){
+            cout << "Twoja odpowiedz: ";
+        }
+        else{
+            cout << "To byla zla odpowiedz.\nTwoja odpowiedz: ";
+        }
+        cin >> answer;
+        count++;
+    } while (answer != "theme:white");
+        clearTerminal();
+        system("Color F0");
+        coutNegative("Brawo, uwolniles ptaszka\n\n");
+        coutNegative(birdFree);
+        coutNegative("\n\n\n");
+        score = 1;
+    return score;
+}
+
+int main() {
+    levelTwo();
+    return 0;
+}
