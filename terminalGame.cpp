9e201badedc7a92e99dccf38ca3a90c7282b5e2b
michalinapepel <misia.pepel@onet.pl>
2024-04-13 22:54:41 +0200

    Umieszczenie w pliku głównym obsługi zapisu postępu gry

--- terminalGame.cpp
+++ terminalGame.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <iomanip>
+#include <fstream>
+#include <cstring>
 #include "./Levels/levelOne.hpp"
 #include "./Levels/levelTwo.hpp"
 #include "./Levels/levelThree.hpp"
@@ -12,18 +14,30 @@ int menu(string name) {
     cout << "!\n\n\n\n\nWybierz:\nNowa gra" << std::setw(7) << "->" << std::setw(6) << "1" << "\nWczytaj gre" << std::setw(4) << "->" << std::setw(6) << "2";
     cin >> choice;
     if (choice == 1){
-        // otworz plik z zapisem postepu gry (jak nie ma to utworz)
-        // wyczysc plik, wpisz 1
+        ofstream gameRecord("gameRecord.txt");
+        gameRecord << "";
+        gameRecord << "1";
+        gameRecord.close(); 
         level = 1;
     }
     else if (choice == 2){
-        // otworz plik z zapisem postepu gry i sprawdz, na ktorym poziomie stanela rozgrywka
-        // jeżeli w pliku nie ma 1, 2 ani 3 to wyświetl "Nie masz jeszcze swojej gry. Stworzyliśmy dla ciebie nową rozgrywkę"
-        // jeżeli jest któraś z tych cyfr to level = liczba z pliku
+        ifstream gameRecord("gameRecord.txt");
+        char output[] = " ";
+        if (gameRecord.is_open()) {
+            gameRecord.get(output[0]);
+            gameRecord.close();
+        }
+        if (strcmp(output, "1") == 0 || strcmp(output, "2") == 0 || strcmp(output, "3") == 0) {
+            level =  std::atoi(output); 
+        }
+        else {
+            cout << "Nie masz jeszcze swojej gry.\nStworzylismy dla ciebie nowa rozgrywke.";
+        }
     }
     
     return level;
 }
+
 int main(int argc, char* argv[]) { 
     string name = argv[1];
     int level = menu(name);
@@ -31,11 +45,15 @@ int main(int argc, char* argv[]) {
     while (true){
         switch(level){
             case 1 : if (levelOne() == 1) {
-                //wpisz do pliku 2
+                ofstream gameRecord("gameRecord.txt");
+                gameRecord << "" << "2";
+                gameRecord.close();
                 level = 2;
             }; break;
             case 2 : if (levelTwo() == 1) {
-                //wpisz do pliku 3
+                ofstream gameRecord("gameRecord.txt");
+                gameRecord << "" << "3";
+                gameRecord.close();
                 level = 3;
             }; break;
             case 3 : if (levelThree() == 1) {
